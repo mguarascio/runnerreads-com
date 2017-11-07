03dec1b23aa2229668c9149e275aacb98806e393
Michael Guarascio <michael.guarascio@gmail.com>
2017-11-06 23:00:36 -0500

    redirect / to /reads/

--- runnerreads/urls.py
+++ runnerreads/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^reads/', include('reads.urls')),
     url(r'^admin/', admin.site.urls),
-    url(r'^v1/api/reads/', include('reads.api'))
+    url(r'^v1/api/reads/', include('reads.api')),
+    url(r'^.*$', RedirectView.as_view(url='reads/', permanent=False), name='index')
 ]
