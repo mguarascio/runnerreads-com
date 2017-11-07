b013973da62057e5b4d9c7a4a1d400e4408bcfb3
Michael Guarascio <michael.guarascio@gmail.com>
2017-11-06 21:32:22 -0500

    added ASIN

--- reads/models.py
+++ reads/models.py
@@ -4,10 +4,11 @@ class Book(models.Model):
     
     title = models.CharField(max_length=255)
     link = models.CharField(max_length=255)
+    ASIN = models.CharField(max_length=20)
     rank = models.IntegerField(null=True)
 
     def __str__(self):
-        return self.title
+        return self.title + ' : ' + self.ASIN
 
 class Comment(models.Model):
     book = models.ForeignKey(Book, related_name='comments', on_delete=models.CASCADE)
--- reads/serializers.py
+++ reads/serializers.py
@@ -10,14 +10,13 @@ class BookSerializer(serializers.HyperlinkedModelSerializer):
     comments = CommentSerializer(many=True)
     class Meta:
         model = Book
-        fields = ('title', 'link', 'rank', 'comments')
+        fields = ('title', 'link', 'ASIN', 'rank', 'comments')
 
     def create(self, validated_data):
             comments_data = validated_data.pop('comments')
-            book, created = Book.objects.get_or_create(link=validated_data['link'])
+            book, created = Book.objects.get_or_create(ASIN=validated_data['ASIN'], link=validated_data['link'], defaults={'title': 'unprocessed', 'rank':9999})
             print('book: ', book)
             print('created? ', created)
             for comment_data in comments_data:
                 Comment.objects.create(book=book, **comment_data)
             return book
-
